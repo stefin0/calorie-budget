042642c82596c4a3871e5d533bc505ec2b1f0a8c
stefin <stefinracho@gmail.com>
2024-02-13 07:30:56 -0800

    feat: add toolbar and recipe making component

--- src/pages/budget/Budget.tsx
+++ src/pages/budget/Budget.tsx
@@ -1,22 +1,17 @@
 import { useState } from "react";
 import useLocalStorage from "../../hooks/useLocalStorage";
 import Style from "./Budget.module.css";
-import Modal from "../../components/modal/Modal";
+import Toolbar from "../../components/toolbar/Toolbar";
 
 function Budget() {
   const [caloriesEaten, setCaloriesEaten] = useLocalStorage(
     "caloriesEaten",
-    "0"
+    "0",
   );
   const [caloriesTotal /*setCaloriesTota*/] = useLocalStorage("bmr", "0");
   const [input, setInput] = useState("");
-  const [showModal, setShowModal] = useState(false);
 
   const caloriesRatio = (caloriesEaten / caloriesTotal) * 100;
-
-  function handleSetShowModal() {
-    setShowModal((prev) => !prev);
-  }
 
   return (
     <>
@@ -24,9 +19,8 @@ function Budget() {
       <div
         className={Style.ring}
         style={{
-          background: `conic-gradient(black ${
-            caloriesRatio * 3.6
-          }deg, grey 0deg)`,
+          background: `conic-gradient(black ${caloriesRatio * 3.6
+            }deg, grey 0deg)`,
         }}
       >
         <span className={Style.pValue}>
@@ -39,13 +33,9 @@ function Budget() {
       </button>
 
       {/* TOOLBAR */}
-      <div className={Style.toolbar}>
-        <button onClick={handleSetShowModal}>Add</button>
-      </div>
-      <h1>{caloriesRatio}</h1>
+      <Toolbar />
 
-      {/* MODAL  */}
-      {showModal && <Modal handleSetShowModal={handleSetShowModal} />}
+      <h1>{caloriesRatio}</h1>
     </>
   );
 }
