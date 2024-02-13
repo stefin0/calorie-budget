bcda0b4d5655b3ba82a4433da5312b8b2f6a1e36
stefin <stefinracho@gmail.com>
2024-02-13 07:38:52 -0800

    refactor: add container around form, and pretty up

--- src/pages/calculator/Calculator.tsx
+++ src/pages/calculator/Calculator.tsx
@@ -13,7 +13,7 @@ function Calculator() {
   const [bmr, setBmr] = useLocalStorage("bmr", "0");
 
   function handleChange(
-    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>,
   ) {
     const { name, value } = e.target;
     setFormData((prevFormData) => ({
@@ -33,62 +33,61 @@ function Calculator() {
   }
 
   return (
-    <form onSubmit={calculateBMR} className={Style.form}>
-      {/*GENDER input*/}
-      <div className={Style.group}>
-        <label htmlFor="gender">Gender</label>
-        <select id="gender" name="gender" onChange={handleChange}>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-        </select>
-      </div>
+    <div className={Style.formContainer}>
+      <form onSubmit={calculateBMR} className={Style.form}>
+        {/*GENDER input*/}
+        <div className={Style.labelInput}>
+          <label htmlFor="gender">Gender</label>
+          <select id="gender" name="gender" onChange={handleChange}>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
 
-      {/*WEIGHT input*/}
-      <div className={Style.group}>
-        <label htmlFor="weight">Weight</label>
-        <input
-          id="weight"
-          name="weight"
-          type="number"
-          placeholder="Current kg"
-          onChange={handleChange}
-          value={formData.weight}
-          required
-        />
-      </div>
+        {/*WEIGHT input*/}
+        <div className={Style.labelInput}>
+          <label htmlFor="weight">Weight</label>
+          <input
+            id="weight"
+            name="weight"
+            type="number"
+            onChange={handleChange}
+            value={formData.weight}
+            required
+          />
+        </div>
 
-      {/*HEIGHT input*/}
-      <div className={Style.group}>
-        <label htmlFor="height">Height</label>
-        <input
-          id="height"
-          name="height"
-          type="number"
-          placeholder="Height (cm)"
-          onChange={handleChange}
-          value={formData.height}
-          required
-        />
-      </div>
+        {/*HEIGHT input*/}
+        <div className={Style.labelInput}>
+          <label htmlFor="height">Height</label>
+          <input
+            id="height"
+            name="height"
+            type="number"
+            onChange={handleChange}
+            value={formData.height}
+            required
+          />
+        </div>
 
-      {/*AGE input*/}
-      <div className={Style.group}>
-        <label htmlFor="age">Age</label>
-        <input
-          id="age"
-          name="age"
-          type="number"
-          placeholder="Age"
-          onChange={handleChange}
-          value={formData.age}
-          required
-        />
-      </div>
+        {/*AGE input*/}
+        <div className={Style.labelInput}>
+          <label htmlFor="age">Age</label>
+          <input
+            id="age"
+            name="age"
+            type="number"
+            onChange={handleChange}
+            value={formData.age}
+            required
+          />
+        </div>
 
-      <button>Calculate BMR</button>
+        <button>Calculate BMR</button>
 
-      <p>Your BMR is {bmr}</p>
-    </form>
+        <p>Your BMR is {bmr}</p>
+      </form>
+    </div>
   );
 }
 
