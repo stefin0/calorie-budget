91d3f2c64e33f8272478073fe6ff1310b944768d
stefin <stefinracho@gmail.com>
2024-02-16 07:55:21 -0800

    feat: add summary to recipe maker

--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -1,7 +1,9 @@
 import { ChangeEvent, useState } from "react";
 import Style from "./Toolbar.module.css";
 import * as AlertDialog from "@radix-ui/react-alert-dialog";
-import { PlusIcon } from "@radix-ui/react-icons";
+import { PlusIcon, Cross2Icon, ChevronDownIcon } from "@radix-ui/react-icons";
+import * as Accordian from "@radix-ui/react-accordion";
+import * as Separator from "@radix-ui/react-separator";
 
 function Toolbar() {
   const [formData, setFormData] = useState({
@@ -29,7 +31,24 @@ function Toolbar() {
         ...prevFormData,
         title: value,
       }));
-    } else if (id) {
+    } else if (
+      name === "quantityAmount" ||
+      name === "fat" ||
+      name === "carb" ||
+      name === "protein"
+    ) {
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        ingredient: prevFormData.ingredient.map((ingredient) =>
+          // prevent user from inputting number < 0
+          ingredient.id === id && Math.abs(+value) > 0
+            ? { ...ingredient, [name]: Math.abs(+value) }
+            : ingredient.id === id && +value <= 0
+              ? { ...ingredient, [name]: value }
+              : ingredient,
+        ),
+      }));
+    } else {
       setFormData((prevFormData) => ({
         ...prevFormData,
         ingredient: prevFormData.ingredient.map((ingredient) =>
@@ -57,9 +76,32 @@ function Toolbar() {
     }));
   }
 
+  function deleteIngredient(id: string) {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      ingredient: prevFormData.ingredient.filter(
+        (ingredient) => ingredient.id !== id,
+      ),
+    }));
+  }
+
   function handleSubmit() {
     console.log(formData);
   }
+
+  const totalFat = formData.ingredient.reduce((total, ingredient) => {
+    return total + +ingredient.fat;
+  }, 0);
+
+  const totalCarb = formData.ingredient.reduce((total, ingredient) => {
+    return total + +ingredient.carb;
+  }, 0);
+
+  const totalProtein = formData.ingredient.reduce((total, ingredient) => {
+    return total + +ingredient.protein;
+  }, 0);
+
+  const totalCalories = 9 * totalFat + 4 * totalCarb + 4 * totalProtein;
 
   return (
     <div className={Style.toolbar}>
@@ -93,6 +135,7 @@ function Toolbar() {
                         type="number"
                         onChange={(e) => handleChange(e, ingredient.id)}
                         value={ingredient.quantityAmount}
+                        min="0"
                         required
                       />
                       <select
@@ -119,19 +162,6 @@ function Toolbar() {
 
                   {/* MACRONUTRIENT row */}
                   <div className={Style.macrosRow}>
-                    {/* CARBOHYDRATE */}
-                    <div className={Style.labelInput}>
-                      <label htmlFor="carb">Carb</label>
-                      <input
-                        id="carb"
-                        name="carb"
-                        type="number"
-                        onChange={(e) => handleChange(e, ingredient.id)}
-                        value={ingredient.carb}
-                        required
-                      />
-                    </div>
-
                     {/* FAT */}
                     <div className={Style.labelInput}>
                       <label htmlFor="fat">Fat</label>
@@ -141,10 +171,26 @@ function Toolbar() {
                         type="number"
                         onChange={(e) => handleChange(e, ingredient.id)}
                         value={ingredient.fat}
+                        min="0"
                         required
                       />
                     </div>
 
+                    {/* CARBOHYDRATE */}
+                    <div className={Style.labelInput}>
+                      <label htmlFor="carb">Carb</label>
+                      <input
+                        id="carb"
+                        name="carb"
+                        type="number"
+                        onChange={(e) => handleChange(e, ingredient.id)}
+                        value={ingredient.carb}
+                        min="0"
+                        required
+                      />
+                    </div>
+
+                    {/* PROTEIN */}
                     <div className={Style.labelInput}>
                       <label htmlFor="protein">Protein</label>
                       <input
@@ -153,18 +199,80 @@ function Toolbar() {
                         type="number"
                         onChange={(e) => handleChange(e, ingredient.id)}
                         value={ingredient.protein}
+                        min="0"
                         required
                       />
                     </div>
                   </div>
+                  {index > 0 && (
+                    <button
+                      className={Style.deleteIngredientButton}
+                      onClick={() => deleteIngredient(ingredient.id)}
+                      aria-label="Delete"
+                    >
+                      <Cross2Icon />
+                    </button>
+                  )}
                 </fieldset>
               ))}
             </div>
 
             {/* NEW INGREDIENT button */}
-            <button className={Style.newIngredientButton} onClick={newIngredient}>
+            <button
+              className={Style.newIngredientButton}
+              onClick={newIngredient}
+            >
               <PlusIcon />
             </button>
+
+            {/* SUMMARY */}
+            <Accordian.Root
+              className={Style.AccordianRoot}
+              type="single"
+              defaultValue="item-1"
+              collapsible
+            >
+              <Accordian.Item value="item-1">
+                <Accordian.Trigger className={Style.AccordionTrigger}>
+                  <span>Summary</span>
+                  <ChevronDownIcon
+                    className={Style.AccordionChevron}
+                    aria-hidden
+                  />
+                </Accordian.Trigger>
+                <Accordian.Content className={Style.AccordianContent}>
+                  {/* TOTAL CALORIES */}
+                  <span
+                    className={`${Style.calories} ${Style.bold}`}
+                    style={{ marginRight: "2rem" }}
+                  >
+                    Calories
+                  </span>
+                  <span className={`${Style.caloriesTotal} ${Style.bold}`}>
+                    {totalCalories}
+                  </span>
+                  <Separator.Root
+                    className={Style.SeparatorRoot}
+                    style={{ height: "3px", marginBottom: "1rem" }}
+                  />
+
+                  {/* TOTAL FAT */}
+                  <span className={Style.bold}>Fat </span>
+                  <span>{totalFat}</span>
+                  <Separator.Root className={Style.SeparatorRoot} />
+
+                  {/* TOTAL CARB */}
+                  <span className={Style.bold}>Carbohydrate </span>
+                  <span>{totalCarb}</span>
+                  <Separator.Root className={Style.SeparatorRoot} />
+
+                  {/* TOTAL PROTEIN */}
+                  <span className={Style.bold}>Protein </span>
+                  <span>{totalProtein}</span>
+                  <Separator.Root className={Style.SeparatorRoot} />
+                </Accordian.Content>
+              </Accordian.Item>
+            </Accordian.Root>
 
             {/* ACTION BUTTONS */}
             <div className={Style.actionButtons}>
