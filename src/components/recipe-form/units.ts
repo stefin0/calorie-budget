a4b73280afcc57701c91e9f7edbd5e4360c791e3
stefin <stefinracho@gmail.com>
2024-02-26 20:10:53 -0800

    fix: display selected unit

--- src/components/recipe-form/units.ts
+++ src/components/recipe-form/units.ts
@@ -1,19 +1,33 @@
 const units = [
-  { value: "liter", label: "L" },
-  { value: "cup", label: "c." },
-  { value: "deciliter", label: "dL" },
-  { value: "fluidOunce", label: "fl oz." },
-  { value: "gallon", label: "gal" },
-  { value: "gram", label: "g" },
-  { value: "kilogram", label: "kg" },
-  { value: "milligram", label: "mg" },
-  { value: "milliliter", label: "mL" },
-  { value: "ounce", label: "oz." },
-  { value: "pint", label: "pt." },
-  { value: "pound", label: "lb." },
-  { value: "quart", label: "qt." },
-  { value: "tablespoon", label: "tbsp." },
-  { value: "teaspoon", label: "tsp." },
+  {
+    category: "Metric",
+    units: [
+      { value: "milligram", label: "mg" },
+      { value: "gram", label: "g" },
+      { value: "kilogram", label: "kg" },
+      { value: "deciliter", label: "dL" },
+      { value: "milliliter", label: "mL" },
+      { value: "liter", label: "L" },
+    ],
+  },
+  {
+    category: "Imperial",
+    units: [
+      { value: "cup", label: "C" },
+      { value: "fluidOunce", label: "fl oz" },
+      { value: "ounce", label: "oz" },
+      { value: "gallon", label: "gal" },
+      { value: "pound", label: "lb" },
+      { value: "pint", label: "pt" },
+      { value: "quart", label: "qt" },
+      { value: "tablespoon", label: "tbsp" },
+      { value: "teaspoon", label: "tsp" },
+    ],
+  },
+  {
+    category: "Other",
+    units: [{ value: "scoop", label: "scoop" }],
+  },
 ];
 
 export default units;
